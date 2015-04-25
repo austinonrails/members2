54bb418614dd9d6bb259fc341f492020fd593e90
Matt E. Patterson <madraziel@gmail.com>
2015-04-25 12:08:06 -0500

    recaptcha why u no work?

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,7 @@
 Recaptcha.configure do |config|
   config.public_key  = ENV['RECAPTCHA_KEY']
   config.private_key = ENV['RECAPTCHA_SECRET']
+  config.api_version = 'v2'
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
